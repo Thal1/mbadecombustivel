458554bbde3be65e2d9ed67597bead443643fa99
Gabriel Inocencio <00001089733987sp@aleducacao.sp.gov.br>
2024-09-11 16:25:14 -0300

    Versão1

--- bomba_de_combustivel.py
+++ bomba_de_combustivel.py
@@ -15,15 +15,5 @@ class BombaCombustivel:
             print(f"Abastecido {litros} litros de {self.tipo_combustivel}. Custo: R${custo:.2f}")
             return custo
 
-    def abastecer_com_aditivo(self, litros):
-        if litros > self.quantidade_combustivel:
-            print(f"Quantidade insuficiente de {self.tipo_combustivel} na bomba.")
-            return 0
-        else:
-            custo = litros * self.preco_com_aditivo
-            self.quantidade_combustivel -= litros
-            print(f"Abastecido {litros} litros de {self.tipo_combustivel} com aditivo. Custo: R${custo:.2f}")
-            return custo
-
-    def verificar_quantidade(self):
-        print(f"Quantidade disponível de {self.tipo_combustivel}: {self.quantidade_combustivel} litros.")
+  
+   
